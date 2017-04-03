f8a76250351a712ff764fc83ad533d48de123241
delromcodes <delromich@gmail.com>
2017-04-02 20:17:51 -0700

    Base Project files

---
+++ Assets/Scripts/MainGame.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainGame : MonoBehaviour {
+
+	public Player Player;
+
+	protected float MoveSensitivity = 0.1f;
+
+	public void Start() {
+		//initialize gameboard
+		//initialize HUD
+	}
+
+	public void Update() {
+		HandleInput();
+	}
+
+	protected void HandleInput() {
+		if(Input.GetKey(KeyCode.LeftArrow)) {
+			Player.Move(new Vector3 (Player.transform.position.x - MoveSensitivity, Player.transform.position.y, Player.transform.position.z));
+		} else if(Input.GetKey(KeyCode.RightArrow)) {
+			Player.Move(new Vector3 (Player.transform.position.x + MoveSensitivity, Player.transform.position.y, Player.transform.position.z));
+		} else if (ShouldStopMoving) {
+			Player.StopMove();
+		}
+	}
+
+	protected bool ShouldStopMoving {
+		get {
+			return Input.GetKey(KeyCode.None) //No keys are being pressed
+				|| Input.GetKeyUp(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow)  //We stopped going left and right wasn't held down
+				|| Input.GetKeyUp(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow); //We stopped going right and left wasn't down
+		}
+	}
+
+	protected void InitializeGameboard() {
+		
+	}
+
+	protected void InitializeHUD() {
+		
+	}
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,105 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public Animator Animator;
+
+	//Animator Triggers
+	protected int scream;
+	protected int strongAttack;
+	protected int basicAttack;
+	protected int getHit;
+	protected int walk;
+	protected int die;
+	protected int run;
+	protected int idle;
+
+	//State related
+	protected bool moving = false;
+
+	//Initialization
+	public void Awake() {
+		SetUpAnimatorHashes();
+	}
+
+	protected void SetUpAnimatorHashes() {
+		if (Animator == null) {
+			Animator = GetComponent<Animator>();
+		}
+		scream = Animator.StringToHash("Scream");
+		basicAttack = Animator.StringToHash("Basic Attack");
+		strongAttack = Animator.StringToHash("Strong Attack");
+		getHit = Animator.StringToHash("Get Hit");
+		walk = Animator.StringToHash("Walk");
+		die = Animator.StringToHash("Die");
+		run = Animator.StringToHash("Run");
+		idle = Animator.StringToHash("Idle");
+	}
+
+	//Public methods
+	public void StopMove() {
+		if (moving) {
+			Debug.Log ("Stop moving");
+			AnimateIdle();
+		}
+	}
+
+	public void Move(Vector3 position) {
+		if (!moving) {
+			Debug.Log ("Move");
+			AnimateRun();
+		}
+		transform.position = position;
+	}
+
+	public void Attack() {
+		AnimateBasicAttack();
+	}
+
+	public void OnHit() {
+		AnimateGetHit();
+	}
+
+	//Attack related
+	protected void AnimateScream() {
+		Animator.SetTrigger(scream);
+	}
+
+	protected void AnimateBasicAttack() {
+		Animator.SetTrigger(basicAttack);
+	}
+
+	protected void AnimateStrongAttack() {
+		Animator.SetTrigger(strongAttack);
+	}
+
+	//Movement related
+	protected void AnimateIdle() {
+		Animator.SetTrigger(idle);
+		moving = false;
+	}
+
+	protected void AnimateWalk() {
+		Animator.SetTrigger(walk);
+		moving = true;
+	}
+
+	protected void AnimateRun() {
+		Animator.SetTrigger(run);
+		moving = true;
+	}
+
+	//Damage related
+	protected void AnimateGetHit() {
+		Animator.SetTrigger(getHit);
+		moving = false;
+	}
+
+	protected void AnimateDie() {
+		Animator.SetTrigger(die);
+		moving = false;
+	}
+}
